c814b5db2fad718d5ae44f92044a979897301eb4
Tristanfrd <Tristanfrd@users.noreply.github.com>
2021-01-16 16:39:27 +0100

    ExpositionRepository méthode implémentée

--- src/main/java/galerie/dao/ExpositionRepository.java
+++ src/main/java/galerie/dao/ExpositionRepository.java
@@ -7,11 +7,19 @@ package galerie.dao;
 
 import galerie.entity.Exposition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
  * @author frede
  */
 public interface ExpositionRepository extends JpaRepository<Exposition, Integer> {
+    
+    @Query(
+    "Select SUM(PRIX_VENTE) as CA FROM exposition"+
+    "INNER JOIN transaction ON Lieu_De_Vente_ID = Exposition.ID"+
+    "WHERE Lieu_De_Vente_ID = :id"
+    )
+    float chiffreAffairePour(Integer id);
     
 }
