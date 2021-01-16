0c612ccdbd20f8dcce72a42eaac6d9009cf5cd68
Tristanfrd <Tristanfrd@users.noreply.github.com>
2021-01-16 14:01:11 +0100

    Jeu de données un peu plus complet

--- src/test/resources/galerie/dao/test-data.sql
+++ src/test/resources/galerie/dao/test-data.sql
@@ -1,2 +1,6 @@
 -- Initialisation spécifiques pour un jeu de test
-INSERT INTO Galerie(nom, adresse) VALUES ('Saatchi', 'King\''s Road, Londres');
+INSERT INTO Galerie(id,nom, adresse) VALUES (1,'Saatchi', 'King\''s Road, Londres');
+
+INSERT INTO Exposition(id,debut,intitule,duree,organisateur_id) VALUES (1,now(), 'Banksy',30,1);
+
+INSERT INTO Personne VALUES ('Client',1,'Castres','FREDERICK',null);
